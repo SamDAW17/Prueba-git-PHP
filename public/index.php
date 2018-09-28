e3beccc0207d4bc2fc0b13bcba218da8b516495d
Samuel Ecija <samuelecija.rod@gmail.com>
2018-09-28 17:11:35 +0000

    Commit inicial

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php $numero = 50 ?>
+
+ <?php if ($numero == 100) { ?>
+ 	 <br> El numero es 100
+<?php } else if ($numero > 100){ ?>
+	 <br> El numero es mayor que 100
+<?php } else if ($numero < 100){ ?>
+	 <br> El numero es menor que 100
+<?php } ?>
